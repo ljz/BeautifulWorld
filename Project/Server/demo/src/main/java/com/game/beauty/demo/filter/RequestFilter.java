14ce4ed78d7ee7ec81e92af65a33c64e24aed3da
ricoyoung345 <yangerming608@hotmail.com>
2018-07-29 11:56:47 +0800

    demo

---
+++ Project/Server/demo/src/main/java/com/game/beauty/demo/filter/RequestFilter.java
@@ -0,0 +1,27 @@
+package com.game.beauty.demo.filter;
+
+import com.game.beauty.demo.log.LogUtil;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class RequestFilter extends HandlerInterceptorAdapter {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("StartTime", startTime);
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        long startTime = (Long) request.getAttribute("startTime");
+        request.removeAttribute("startTime");
+        long endTime = System.currentTimeMillis();
+        LogUtil.info("RequestFilter time consume: " + (endTime - startTime) + ", method: " + request.getMethod() + ", uri:" + request.getRequestURI());
+    }
+}
