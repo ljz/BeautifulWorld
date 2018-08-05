787fb30970e98292370d5630db248a0a64f4b1c1
ricoyoung345 <yangerming608@hotmail.com>
2018-08-05 13:11:39 +0800

    uuid

--- Project/Server/uuid-factory/src/main/java/com/game/beauty/uuidfactory/config/ScopeConfig.java
+++ Project/Server/uuid-factory/src/main/java/com/game/beauty/uuidfactory/config/ScopeConfig.java
@@ -1,4 +1,9 @@
 package com.game.beauty.uuidfactory.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.game.beauty.uuidfactory")
 public class ScopeConfig {
 }
